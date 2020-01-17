b9a96c1b7ec0a720ef0a47e7b529c44e217c4062
anfractuosity <anfractuosity@anfractuosity.com>
2020-01-17 14:31:11 +0000

    commenting code

--- tag_leds.py
+++ tag_leds.py
@@ -24,23 +24,9 @@ WIDTH = 1280
 HEIGHT = 720
 FPS = 50
 ZERO_COLOUR = (0, 0, 255)
-ONE_COLOUR = (0, 255, 0)
+TIME_THRESHOLD = 300 # milliseconds
 
-old = -1
-frameno = 0
-allleds = []
-
-x = 10
-y = 10
-idcount = 0
-fps = 1
-fcount = 1
-lastf = None
-uid = 0
-
-allh = {}
-frame = 0
-
+# LED representation
 class LED(Sequence):
     def __init__(self, x, y, tim, idv):
         self.x = x
@@ -67,7 +53,8 @@ class LED(Sequence):
     def addtimestamp(self, tstamp):
         self.timestamp.append(tstamp)
 
-def blobDet(val, frame):
+# Detect blobs in frame
+def blobdetect(frame):
     lthres = 140
     ret, frame = cv2.threshold(frame, lthres, 255, 0)
     params = cv2.SimpleBlobDetector_Params()
@@ -77,30 +64,22 @@ def blobDet(val, frame):
     params.filterByCircularity = False
     params.filterByConvexity = False
     params.filterByInertia = False
-    params.maxThreshold = 255
     params.filterByColor = True
     params.blobColor = 255
-
     detector = cv2.SimpleBlobDetector_create(params)
-    keypoints = detector.detect(frame)
-    return keypoints
+    return detector.detect(frame)
 
-def additem(led):
-    allh[led.idv] = led
-
-def nearesti(search, dist):
+# Find nearest potential LED
+def nearesti(allh,search, dist):
     found = []
-
     for idv in allh:
-
         ldv = allh[idv]
         dst = math.sqrt((search.x - ldv.x) ** 2 + (search.y - ldv.y) ** 2)
-
         if dst < dist:
             found.append((dst, ldv))
-
     return sorted(found, key=itemgetter(0))
 
+# Sliding window
 def window(iterable, size):
     iters = tee(iterable, size)
     for i in range(1, size):
@@ -116,6 +95,7 @@ def binary(arr):
         m = m + 1
     return s
 
+# Manchester decode
 def mancdec(arr):
     out = []
     for i in range(0, len(arr), 2):
@@ -125,126 +105,136 @@ def mancdec(arr):
             out.append(1)
     return out
 
-def getbits(p2):
+# Extract bits from frame differences, this then needs to be decoded
+# using the mancdec function
+def getbits(framediffs):
 
     nout = 0
     bits = []
-    for v in p2:
+    f = ((1 / FPS) * 2) * 1000
 
-        if v < 35:
+    for framediff in framediffs:
+        if framediff < f:
             nout = nout + 1
         else:
-            if nout <= 16:
+            if nout*((1/FPS)*1000) <= TIME_THRESHOLD:
                 bits.append(1)
             else:
                 bits.extend([1, 1])
 
-            off = v
-
-            if off <= 300:
+            if framediff <= TIME_THRESHOLD:
                 bits.append(0)
             else:
                 bits.extend([0, 0])
 
             nout = 1
+
     return bits
 
+
+# Calculate CRC checksum
 def crc(val):
     s = ""
     for b in val:
         s = s + str(b)
     return binascii.crc32(s.encode("ascii")) % 256
 
-cap = cv2.VideoCapture("lights.MOV")
 
-while True:
-    timestamp = int(cap.get(cv2.CAP_PROP_POS_MSEC))
+# Get blobs from video frames
+def getblobs(videofile):
 
-    fcount = fcount + 1
-    if fcount > FPS:
-        frameno = frameno + 1
-        if timestamp > old + 1000:
-            if frameno > 1:
+    cap = cv2.VideoCapture(videofile)
+    potentialleds = {}
+    blobframes = []
+    fcount = 0
+    fps = 0
+    frameno = 0
+    old = -1
+    uid = 0
+
+    while True:
+
+        # Calculate framerate
+        timestamp = int(cap.get(cv2.CAP_PROP_POS_MSEC))
+        fcount = fcount + 1
+        if fcount > FPS:
+            frameno = frameno + 1
+            if timestamp > old + 1000 and frameno > 1:
                 print("FPS ", frameno)
                 fps = frameno
                 frameno = 0
                 old = timestamp
 
-    ret, frame = cap.read()
+        ret, frame = cap.read()
+        if type(frame) != np.ndarray:
+            break
 
-    if type(frame) != np.ndarray:
-        break
-    else:
+        # Find blobs, and draw them on image
         lastf = frame
-
-    try:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    except cv2.error:
-        break
+        blobs = blobdetect(frame)
+        im = cv2.drawKeypoints(
+            frame,
+            blobs,
+            np.array([]),
+            ZERO_COLOUR,
+            cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+        )
 
-    z = blobDet(0, frame)
-    im = cv2.drawKeypoints(
-        frame,
-        z,
-        np.array([]),
-        ZERO_COLOUR,
-        cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
-    )
-    cv2.imshow("", im)
+        cv2.imshow("", im)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
-    cur = []
+        cur = []
+        if fps > 20:
+            for k in blobs:
+                x = k.pt[0]
+                y = k.pt[1]
+                cur.append((x, y, k.size))
 
-    if fps > 20:
-        for k in z:
-            x = k.pt[0]
-            y = k.pt[1]
-            cur.append((x, y, k.size))
+            blobframes.append({"timestamp": timestamp, "points": cur})
 
-        allleds.append({"timestamp": timestamp, "points": cur})
+            # Process blobs in current frame
+            for l in cur:
+
+                uid = uid + 1
+                search_led = LED(l[0], l[1], timestamp, uid)
+                nearest = nearesti(potentialleds, search_led, 8)
+
+                # If no previous potential LED, add this LED to potential LEDs
+                if len(nearest) == 0:
+                    potentialleds[search_led.idv] = search_led
+                    continue
+
+                # If we found a potential previous LED, add this timestamp to it
+                add = True
+                for allv in nearest:
+                    if allv[1].timestamp[-1] != timestamp:
+                        o = allv[1]
+                        o.addtimestamp(timestamp)
+                        o.x = search_led.x
+                        o.y = search_led.y
+                        potentialleds[o.idv] = o
+                        add = False
+                        break
+
+                if add:
+                    potentialleds[search_led.idv] = search_led
+
+    return lastf, potentialleds
 
 print("Analysing")
 frame = 0
-
-for i in allleds:
-    frame = frame + 1
-
-    if frame % 10:
-        print("Complete ", (frame / len(allleds)) * 100, " --- len ", len(allh.keys()))
-
-    for l in i["points"]:
-        uid = uid + 1
-        ld = LED(l[0], l[1], i["timestamp"], uid)
-
-        nearest = nearesti(ld, 8)
-
-        if len(nearest) == 0:
-            additem(ld)
-            continue
-
-        add = True
-        for allv in nearest:
-            if allv[1].timestamp[-1] != i["timestamp"]:
-                old = allv[1]
-                old.addtimestamp(i["timestamp"])
-                old.x = ld.x
-                old.y = ld.y
-                additem(old)
-                add = False
-                break
-
-        if add == True:
-            additem(ld)
-
 mp = IDs(0).manchester(IDs(0).preamble)
-tups = {}
+tagged_leds = {}
 idd = 0
 
-for key in allh:
-    t = allh[key].timestamp
-    led = allh[key]
+lastf, potentialleds = getblobs("lights.MOV")
+
+for key in potentialleds:
+    t = potentialleds[key].timestamp
+    led = potentialleds[key]
 
     if len(t) > 20:
         t2 = [t[i] for i in range(1, len(t))]
@@ -256,7 +246,6 @@ for key in allh:
             old = v
 
         bits = getbits(out)
-        marker = 0
         found = False
 
         most = defaultdict(int)
@@ -267,7 +256,7 @@ for key in allh:
             if pre == mp:
                 m = mancdec(n2)
                 check = binary(m[-8:])
-                bbb = binary(m[16 : 16 + 10])
+                bbb = binary(m[16: 16 + 10])
 
                 if crc(m[0 : 16 + 10]) == check:
                     most[bbb] = most[bbb] + 1
@@ -283,19 +272,18 @@ for key in allh:
                 last = most[k]
 
         if found:
-            tups[idv] = tup
+            tagged_leds[idv] = tup
 
-        marker = marker + 1
-
-ct = 0
-for k in tups:
-    ct += 1
-    idv = tups[k]
+# Display tagged LEDs on last processed video frame
+numleds = 0
+for led_id in tagged_leds:
+    numleds += 1
+    led_pos = tagged_leds[led_id]
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(
         lastf,
-        str(k),
-        (int(idv[0]), int(idv[1])),
+        str(led_id),
+        (int(led_pos[0]), int(led_pos[1])),
         font,
         0.3,
         (0, 0, 255),
@@ -303,8 +291,8 @@ for k in tups:
         cv2.LINE_AA,
     )
 
-print("found ", ct)
-pickle.dump(tups, open("save.p", "wb"))
+print("found ", numleds)
+pickle.dump(tagged_leds, open("save.p", "wb"))
 
 cv2.imshow("Tagged image", lastf)
 cv2.waitKey(0)
